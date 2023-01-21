e2962a1612a351bb507bd353abf51fc58d94c33b
Makerspace1 <123253544+Makerspace1@users.noreply.github.com>
2023-01-21 16:43:54 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+function SendMessage (message: string) {
+    for (let index = 0; index <= Math.ceil(message.length / maxMessageLength) - 1; index++) {
+        radio.sendString(message.substr(index * maxMessageLength, maxMessageLength))
+    }
+    radio.sendString("" + ("\n"))
+    sendBytes += serialReceivedString.length
+}
+radio.onReceivedString(function (receivedString) {
+    radioRecBuffer = "" + radioRecBuffer + receivedString
+    while (radioRecBuffer.includes("\n")) {
+        serial.writeLine(radioRecBuffer.substr(0, radioRecBuffer.indexOf("\n")))
+        radioRecBuffer = radioRecBuffer.substr(radioRecBuffer.indexOf("\n") + 1, 0)
+    }
+    recBytes += receivedString.length
+})
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    serialReceivedString = serial.readString()
+    SendMessage(serialReceivedString)
+})
+let recBytes = 0
+let radioRecBuffer = ""
+let serialReceivedString = ""
+let sendBytes = 0
+let maxMessageLength = 0
+maxMessageLength = 18
+radio.setGroup(1)
+serial.setRxBufferSize(128)
+serial.setTxBufferSize(128)
+serial.writeLine("GW: Rover gateway started")
